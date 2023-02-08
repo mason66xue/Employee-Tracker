c841bb5f4ed2a241dc513cd45332b4f2993946e6
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-07 23:12:08 -0500

    modification of shema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,39 @@
-DROP DATABASE IF EXISTS cms_db;
-CREATE DATABASE: cms_db;
 
-USE cms_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+
+USE company_db;
+
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
-  
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL
- salary DECIMAL NOT NULL, 
-   department_id INT NOT NULL,
-  CONSTRAINT fk_department FOREIGN KEY(department_id)
-  REFERENCES department(id) ON DELETE SET NULL 
-
-  
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  
-  id INT SET NULL AUTO_INCREMENT PRIMARY KEY,
- first_name VARCHAR(30) NOT NULL,
- last_name VARCHAR(30) NOT NULL,
- role_id INT NOT NULL,
-  manager_id INT
-   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
-  
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT DEFAULT NULL,
+  manager_id INT DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
